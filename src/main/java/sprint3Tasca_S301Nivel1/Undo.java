cdfc40c86943c6a0e185ef57edb93ae522192147
jrffruit518-enrich <jrffruit518@gmail.com>
2025-11-12 12:59:39 +0100

    fix : update Undo.java

--- src/main/java/sprint3Tasca_S301Nivel1/Undo.java
+++ src/main/java/sprint3Tasca_S301Nivel1/Undo.java
@@ -23,6 +23,7 @@ public class Undo {
     public void undoCommand() {
         if (!this.commands.isEmpty()) {
             this.commands.removeLast();
+            System.out.println("The last command has been undo.");
         } else {
             System.out.println("There is no command to undo.");
         }
@@ -30,7 +31,9 @@ public class Undo {
 
     public void showCommandHistory() {
         if (!this.commands.isEmpty()) {
+            System.out.println("*************");
             this.commands.forEach(System.out::println);
+            System.out.println("*************");
         } else {
             System.out.println("There is no command to undo.");
         }
