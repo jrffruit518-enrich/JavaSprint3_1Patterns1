8488c19f80fbb61c353a0250b0ee5a04f4defde4
jrffruit518-enrich <jrffruit518@gmail.com>
2025-11-12 13:42:53 +0100

    fix : update UndoMain.java

--- src/main/java/sprint3Tasca_S301Nivel1/UndoMain.java
+++ src/main/java/sprint3Tasca_S301Nivel1/UndoMain.java
@@ -2,6 +2,7 @@ package sprint3Tasca_S301Nivel1;
 
 public class UndoMain {
     public static void main(String[] args) {
-        Opration.startProgram();
+        Operation operation = new Operation();
+        operation.startProgram();
     }
 }
