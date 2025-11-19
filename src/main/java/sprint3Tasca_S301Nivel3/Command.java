523852aa666d13c6dd700c57ec7eb234a1713647
jrffruit518-enrich <jrffruit518@gmail.com>
2025-11-19 10:53:12 +0100

    feat : initial commit

---
+++ src/main/java/sprint3Tasca_S301Nivel3/Command.java
@@ -0,0 +1,6 @@
+package sprint3Tasca_S301Nivel3;
+
+public interface Command {
+    public void execute();
+    public void undo();
+}
---
+++ src/main/java/sprint3Tasca_S301Nivel3/Vehicle.java
@@ -0,0 +1,7 @@
+package sprint3Tasca_S301Nivel3;
+
+public interface Vehicle {
+    public void start();
+    public void accelerate();
+    public void brake();
+}
