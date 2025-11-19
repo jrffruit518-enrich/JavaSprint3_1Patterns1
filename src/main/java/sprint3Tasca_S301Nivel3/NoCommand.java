ca8cbfadca66019352c1276f8af288d95c74ecaf
jrffruit518-enrich <jrffruit518@gmail.com>
2025-11-19 10:53:53 +0100

    feat : add NoCommand.java

---
+++ src/main/java/sprint3Tasca_S301Nivel3/NoCommand.java
@@ -0,0 +1,13 @@
+package sprint3Tasca_S301Nivel3;
+
+public class NoCommand implements Command{
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
+}
